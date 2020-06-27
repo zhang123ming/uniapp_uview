7a93adf15da3e6784fbb016c7a179cdb98547a01
unknown <feng@qq.com>
2020-06-27 18:04:22 +0800

    6-27/

--- pages/switch/switch.vue
+++ pages/switch/switch.vue
@@ -1,21 +1,43 @@
 <template>
-	<view>{{ name }}</view>
+	<view class="switch"><u-switch v-model="checked" @change="change" :loading="isTrue"></u-switch></view>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			name: ''
+			checked: false,
+			isTrue: false
 		};
 	},
 	onLoad(e) {
-		this.name = e.option;
 		uni.setNavigationBarTitle({
 			title: e.option
 		});
+	},
+	methods: {
+		change(e) {
+			const self=this;
+			if (e) {
+				self.isTrue = true;
+				setTimeout(function() {
+					
+					self.isTrue = false;
+				}, 2000);
+			} else {
+				self.isTrue = false;
+			}
+		}
 	}
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.switch {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
